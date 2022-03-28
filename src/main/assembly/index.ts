afdbaf32ab9b6d449c10f3f9ab9c1f981c00a612
doc_failure <stefano.convertino@gmail.com>
2022-03-28 17:05:18 +0200

    Fix nft_metadata and nft_initialize

--- src/main/assembly/index.ts
+++ src/main/assembly/index.ts
@@ -7,13 +7,13 @@ const base64TokenSvg:string="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wI
 const metadata:TokenMetadata={
   title: "string", // ex. "Arch Nemesis: Mail Carrier" or "Parcel #5055"
   description: "string", // free-form description
-  media: "string", // URL to associated media, preferably to decentralized, content-addressed storage
+  media: "https://bafkreiaxotuu535stf35clnaz57obf7zjipvxbr7hiv4iu5o6gin52mljm.ipfs.dweb.link/", // URL to associated media, preferably to decentralized, content-addressed storage
   media_hash: "string", // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
-  copies: 8, // number of copies of this set of metadata in existence when token was minted.
-  issued_at: 8, // When token was issued or minted, Unix epoch in milliseconds
-  expires_at: 8, // When token expires, Unix epoch in milliseconds
-  starts_at: 8, // When token starts being valid, Unix epoch in milliseconds
-  updated_at: 8, // When token was last updated, Unix epoch in milliseconds
+  copies: 1, // number of copies of this set of metadata in existence when token was minted.
+  issued_at: 0, // When token was issued or minted, Unix epoch in milliseconds
+  expires_at: 0, // When token expires, Unix epoch in milliseconds
+  starts_at: 0, // When token starts being valid, Unix epoch in milliseconds
+  updated_at: 0, // When token was last updated, Unix epoch in milliseconds
   extra: "string", // anything extra the NFT wants to store on-chain. Can be stringified JSON.
   reference: "string", // URL to an off-chain JSON file with more info.
   reference_hash: "string" // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
@@ -88,7 +88,7 @@ export function nft_mint_batch(owner_ids: string[], metadata: TokenMetadata): vo
 }
 
 export function nft_metadata():NFTContractMetadata | null{
-  return new NFTContractMetadata("nft-1.0.0", "name", "symbol", base64TokenSvg, "", "", "");
+   return nft_metadata_impl();
 }
 
   // Query for all the tokens for an owner
